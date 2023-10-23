387d7cf96b90396214d175edabc5a21c1da73881
AgapAnd <braniganzepp@gmail.com>
2023-10-24 00:03:04 +0300

    made task

--- src/main/java/ru/agapov/sber/Main.java
+++ src/main/java/ru/agapov/sber/Main.java
@@ -1,7 +1,36 @@
 package ru.agapov.sber;
 
+import ru.agapov.sber.models.City;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        readCatalog().forEach(System.out::println);
+    }
+
+    public static List<City> readCatalog() throws IOException {
+        List<City> cities = new ArrayList<>();
+        Scanner scan = new Scanner(new File("src/resources/sber-catalog.csv"));
+        while (scan.hasNextLine()) {
+
+            String[] array;
+            array = scan.nextLine().split(";");
+            City city = new City(
+                    Integer.parseInt(array[0])
+                    ,array[1]
+                    ,array[2]
+                    ,array[3]
+                    ,Long.parseLong(array[4])
+                    ,array[5]);
+            cities.add(city);
+        }
+        return cities;
 
     }
 }
