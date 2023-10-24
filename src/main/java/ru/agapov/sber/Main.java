44c4821110958da75c320c25b46c2a8737621a5a
AgapAnd <braniganzepp@gmail.com>
2023-10-24 10:33:38 +0300

    last_version

--- src/main/java/ru/agapov/sber/Main.java
+++ src/main/java/ru/agapov/sber/Main.java
@@ -11,26 +11,33 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        readCatalog().forEach(System.out::println);
+        Scanner scan = readCatalog(new File("src/resources/sber-catalog.csv"));
+        extractData(scan).forEach(System.out::println);
     }
 
-    public static List<City> readCatalog() throws IOException {
+    public static Scanner readCatalog(File file) throws IOException {
+        Scanner scan = new Scanner(file);
+        return scan;
+    }
+    public static List<City> extractData(Scanner scan) {
         List<City> cities = new ArrayList<>();
-        Scanner scan = new Scanner(new File("src/resources/sber-catalog.csv"));
         while (scan.hasNextLine()) {
+            String[] array = scan.nextLine().split(";",0);
+            String foundation;
+            if (array.length == 6)
+                foundation = array[5];
+            else
+                foundation = "\u001B[33mнет данных\u001B[0m";
 
-            String[] array;
-            array = scan.nextLine().split(";");
             City city = new City(
-                    Integer.parseInt(array[0])
-                    ,array[1]
-                    ,array[2]
-                    ,array[3]
-                    ,Long.parseLong(array[4])
-                    ,array[5]);
-            cities.add(city);
+                        Integer.parseInt(array[0])
+                        , array[1]
+                        , array[2]
+                        , array[3]
+                        , Long.parseLong(array[4])
+                        , foundation);
+                cities.add(city);
         }
         return cities;
-
     }
 }
